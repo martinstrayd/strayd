61bf5cb4177b0de6e780a6cabad7cbf723c60bc6
Strayd <martinstrayd@gmail.com>
2026-05-03 11:29:58 +0700

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
+import fetch from 'node-fetch';
 
-const app = express();
+export default async function handler(req, res) {
+  const { query } = req;
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'https://www.strayd.com/api/auth/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  done(null, { accessToken, profile });
-}));
+  if (query.code) {
+    // Exchange code for access token
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: query.code,
+        redirect_uri: 'https://www.strayd.com/api/auth'
+      })
+    });
 
-app.get('/auth', passport.authenticate('github'));
-app.get('/auth/callback', passport.authenticate('github', { failureRedirect: '/' }),
-  (req, res) => {
-    res.json({ token: req.user.accessToken });
-  });
+    const data = await response.json();
+    return res.status(200).json(data);
+  }
 
-module.exports = app;
+  // Redirect user to GitHub OAuth
+  const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=https://www.strayd.com/api/auth`;
+  res.redirect(redirect);
+}
