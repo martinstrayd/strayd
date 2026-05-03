a5762fb924fc04ca03dea0137006397695b85c1b
Strayd <martinstrayd@gmail.com>
2026-05-03 14:05:29 +0700

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -1,10 +1,34 @@
+import fetch from "node-fetch";
+
+async function getAccessToken() {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch token: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.access_token;
+}
+
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.spotify.com/v1/search?q=martin%20strayd&type=artist", {
-      headers: {
-        Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`
+    const token = await getAccessToken();
+
+    const response = await fetch(
+      "https://api.spotify.com/v1/albums/4ZfgXJa22P6r6toOQURyvs", // Devils Goin Public
+      {
+        headers: { Authorization: `Bearer ${token}` }
       }
-    });
+    );
 
     if (!response.ok) {
       const errorText = await response.text();
