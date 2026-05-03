3dd31f88c4a88720411de68ab56e6058090d36b8
Strayd <martinstrayd@gmail.com>
2026-05-03 11:34:49 +0700

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -28,3 +28,4 @@ export default async function handler(req, res) {
   const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=https://www.strayd.com/api/auth`;
   res.redirect(redirect);
 }
+ 
