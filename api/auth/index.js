d01434c4579214c3595abab0cff095b312d0a8f3
Strayd <martinstrayd@gmail.com>
2026-05-03 11:33:41 +0700

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -5,7 +5,10 @@ export default async function handler(req, res) {
     // Exchange code for access token
     const response = await fetch('https://github.com/login/oauth/access_token', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
       body: JSON.stringify({
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -15,10 +18,13 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    return res.status(200).json(data);
+
+    // Redirect back to CMS admin with token in URL fragment
+    const redirectUrl = `https://www.strayd.com/cms/admin/cms.html#access_token=${data.access_token}&token_type=${data.token_type}&scope=${data.scope}`;
+    return res.redirect(redirectUrl);
   }
 
-  // Redirect user to GitHub OAuth
+  // Initial step: send user to GitHub OAuth
   const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=https://www.strayd.com/api/auth`;
   res.redirect(redirect);
 }
