032726e7b35e24d1581e327b4e606703a9d81d3b
Strayd <martinstrayd@gmail.com>
2026-05-03 14:03:12 +0700

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -1,13 +1,10 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      "https://api.spotify.com/v1/albums/4ZfgXJa22P6r6toOQURyvs", // Devils Goin Public
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`
-        }
+    const response = await fetch("https://api.spotify.com/v1/search?q=martin%20strayd&type=artist", {
+      headers: {
+        Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`
       }
-    );
+    });
 
     if (!response.ok) {
       const errorText = await response.text();
